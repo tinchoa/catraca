b6570906eeeb6afa568bd37522ec8164db4eb4d3
Tincho <martin@gta.ufrj.br>
2018-03-07 20:37:29 +0100

    Update test.py

--- processing-layer/test.py
+++ processing-layer/test.py
@@ -122,56 +122,16 @@ def CorrelationFeature(vectors):
 
 	return vectors2 #matriz reducida
 
-def pass2libsvm(vectors2):
+def pass2libsvm(vectors2,classes):
 
-	###to make the reduced matrix with vectors
-	dif1=[]
-	#dif1 = [0]*len(vectors)
-	z={}
-	z[1]=[]
-	dif2=[]
-	#dif2 = [0]*len(vectors)
-	z[2]=[]
+	newVector=classes.zip(vectors2)
+	grouped=newVector.groupByKey().mapValues(list)
+	final=newVector.map(lambda x : LabeledPoint(x[0],x[1]))
 
-	dif3=[]
-	z[3]=[]
-	#dif3 = [0]*len(vectors)
-	e=[]
-	for i in range(len(vectors2)):
-			if sys.argv[1] == '1': # dataset Renato
-				if int(classes[i]) == 0:
-					dif1.append(vectors2[i])
-					e.append(LabeledPoint(0,np.array(dif1)))
-					dif1=[]
-				if int(classes[i]) == 1:
-					dif2.append(vectors2[i])
-					e.append(LabeledPoint(1,np.array(dif2)))
-					dif2=[]
-			else:
-				
-				if int(classes[i]) == 1:
-					dif1.append(vectors2[i])
-					e.append(LabeledPoint(0,np.array(dif1)))
-					dif1=[]
-				if int(classes[i]) == 2:
-					dif2.append(vectors2[i])
-					e.append(LabeledPoint(1,np.array(dif2)))
-					dif2=[]
-				if int(classes[i]) == 3:
-					dif3.append(vectors2[i])
-					e.append(LabeledPoint(2,np.array(dif3)))
-					dif3=[]
-
-
-		#ver como hacer el tema de la libsvm list
-		#deveria ser algo del tipo 1, () ,2 (), 1 (), 3 (), 2()
-
+	
 	print 'returning libsvm format'
-	final=sc.parallelize(e) #return in libsvm format
-
 
 	return final
-
 #to save file in disk
 
 #tempFile = NamedTemporaryFile(delete=True)
