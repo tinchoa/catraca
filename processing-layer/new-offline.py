6181512a1115a0208168ccddb806a5fbe1f5c6dc
Tincho <martin@gta.ufrj.br>
2018-03-08 17:30:09 +0100

    Update new-offline.py

--- processing-layer/new-offline.py
+++ processing-layer/new-offline.py
@@ -43,7 +43,7 @@ def CorrelationFeature(vectors):
 	
 	print 'Calculation Correlation'
 	
-	matriz=sc.broadcast(Statistics.corr(sc.parallelize(vectors), method="pearson"))
+	matriz=sc.broadcast(Statistics.corr(vectors, method="pearson"))
 
 	summary = Statistics.colStats(vectors)
 
